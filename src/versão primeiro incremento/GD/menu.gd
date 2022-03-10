2e80b15b7bce796417a4a16b7020f6e1bbb27c8b
Gabi-Barretto <99404153+Gabi-Barretto@users.noreply.github.com>
2022-03-10 17:15:59 -0300

    Delete src/versão primeiro incremento directory

--- src/versão primeiro incremento/GD/menu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-#Botão de sair do jogo
-func _on_Sair_pressed():
-	get_tree().quit()
-
-#Botão de começar o jogo
-func _on_Start_pressed():
-	get_tree().change_scene("res://TSCN/Feira.tscn")
