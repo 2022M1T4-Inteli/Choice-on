605cd482e5facce5d6a37cf74a47ea592706974b
Gabi-Barretto <99404153+Gabi-Barretto@users.noreply.github.com>
2022-03-11 17:11:13 -0300

    Update Inimigo1.gd

--- src/Scripts/Inimigo1.gd
+++ src/Scripts/Inimigo1.gd
@@ -5,22 +5,22 @@ const GRAVITY = 25
 
 var motion = Vector2(0,0)
 var up = Vector2(0,-1)
-var direction = 1
+var direction = 1 #variavel para indicar direcao do mob
 
 
 func _physics_process(delta):
 
-	if is_on_wall() == true:
+	if is_on_wall() == true: #quando o personagem entra em contato com uma parede, muda o valor da variavel.
 
 		if direction == 1:
 			direction = 0
 		else:
 			direction = 1
 
-	if direction == 1:
+	if direction == 1:		#essa condicao delimitada pela variavel direction, aplica o devido movimento ao mob
 		motion.x = SPEED
 	else:
-		motion.x = SPEED * -1
+		motion.x = -SPEED 
 
 	motion.y += GRAVITY
 
