33eab30cf326cf6c73b32257a23e35839834955c
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2022-03-22 09:40:35 -0300

    Jogo - 1.3

--- src/JOGO.CHOICE/Scripts/AreaTenda.gd
+++ src/JOGO.CHOICE/Scripts/AreaTenda.gd
@@ -7,7 +7,12 @@ func _ready():
 func _on_AreaTenda_body_entered(body):
 	body.onTenda()
 	$TextEdit.visible = true
+	$AnimatedSprite.visible = false
+	$AnimatedSprite2.visible = false
+	
 
 func _on_AreaTenda_body_exited(body):
 	$TextEdit.visible = false
+	$AnimatedSprite.visible = true
+	$AnimatedSprite2.visible = true
 	pass # Replace with function body.
--- src/JOGO.CHOICE/Scripts/Cena1.gd
+++ src/JOGO.CHOICE/Scripts/Cena1.gd
@@ -3,3 +3,14 @@ extends Node2D
 func _ready():
 	Classglobal.Scene = "Cena 1"
 	pass 
+
+func _process(delta):
+#	if Classglobal.Scene == "Cena 1":
+		if Input.is_key_pressed(KEY_M):
+			$Player/Camera2D2.current = true
+			$Player/Sprite.visible = true
+			$Player/Sprite2.visible = true
+		elif Input.is_key_pressed(KEY_P):
+			$Player/Camera2D.current = true
+			$Player/Sprite.visible = false
+			$Player/Sprite2.visible = false
