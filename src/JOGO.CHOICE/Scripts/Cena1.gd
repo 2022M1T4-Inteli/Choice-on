8805adc22afa00998b05b8bec1d77daab878fbd4
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2022-03-22 10:49:21 -0300

    1.3

--- src/JOGO.CHOICE/Scripts/Cena1.gd
+++ src/JOGO.CHOICE/Scripts/Cena1.gd
@@ -10,7 +10,12 @@ func _process(delta):
 			$Player/Camera2D2.current = true
 			$Player/Sprite.visible = true
 			$Player/Sprite2.visible = true
+			if $Player.position.x >= 7850:
+				$Player/Camera2D2.limit_bottom = 4700
 		elif Input.is_key_pressed(KEY_P):
 			$Player/Camera2D.current = true
 			$Player/Sprite.visible = false
 			$Player/Sprite2.visible = false
+			
+
+
